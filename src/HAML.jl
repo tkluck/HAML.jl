784c146229f55feed2c4ebf2728307c3158d1f23
Timo Kluck <tkluck@infty.nl>
2020-03-11 16:33:41 +0100

    Remove custom attempt at Revise-like workflow

--- src/HAML.jl
+++ src/HAML.jl
@@ -10,10 +10,10 @@ include("Helpers.jl")
 
 import .Codegen: generate_haml_writer_codeblock, @haml_str, @io, @output
 import .Parse: Source
-import .Templates: render, @include
+import .Templates: render, @include, includehaml
 import .Helpers: @surround, @precede, @succeed
 
-export @haml_str, render, @output, @io, @include
+export @haml_str, render, @output, @io, @include, includehaml
 export @surround, @precede, @succeed
 
 end # module
--- src/Templates.jl
+++ src/Templates.jl
@@ -4,76 +4,7 @@ import HAML
 
 import ..Hygiene: make_hygienic, invert_escaping, replace_expression_nodes_unescaped
 import ..Parse: Source
-import ..Codegen: generate_haml_writer_codeblock, replace_output_nodes
-
-struct FileRevision{Dir, Basename, MTime} end
-
-Base.dirname(::FileRevision{Dir, Basename, MTime}) where {Dir, Basename, MTime}      = string(Dir)
-Base.basename(::FileRevision{Dir, Basename, MTime}) where {Dir, Basename, MTime} = string(Basename)
-
-openat(dirname, filename) = open(joinpath(dirname, filename))
-
-const open_files = Dict()
-
-function Base.open(fr::FileRevision)
-    openat(dirname(fr), basename(fr))
-end
-
-function Base.Symbol(fr::FileRevision)
-    return Symbol(joinpath(dirname(fr), basename(fr)))
-end
-
-module Generated end
-
-function render end
-
-const module_template = quote
-    import HAML: @io, @output, @include
-end
-
-function getmodule(filerevision)
-    name = Symbol(repr(filerevision))
-    try
-        return getproperty(Generated, name)
-    catch
-        Base.eval(Generated, :( module $name; $module_template; end ))
-        return getproperty(Generated, name)
-    end
-end
-
-function FileRevision(dirname, basename)
-    st = stat(openat(dirname, basename))
-    if !iszero(st.inode)
-        return FileRevision{Symbol(dirname), Symbol(basename), st.mtime}()
-    else
-        error("Cannot read file information")
-    end
-end
-
-@generated function render(io::IO, ::FR, ::Val{indent}; variables...) where FR <: FileRevision where indent
-    usermod = getmodule(FR())
-    source = read(open(FR()), String)
-    sourceref = LineNumberNode(1, Symbol(FR()))
-    code = generate_haml_writer_codeblock(usermod, Source(sourceref, source), string(indent))
-    code = replace_output_nodes(code, :io)
-    code = replace_expression_nodes_unescaped(:$, code) do sym
-        sym isa Symbol || error("Can only use variables as interpolations")
-        :( variables.data.$sym )
-    end
-    code = invert_escaping(code)
-    code = make_hygienic(usermod, code)
-    return code
-end
-
-function render(io::IO, basename::AbstractString, dirname::AbstractString; indent=Val(Symbol("")), variables=())
-    fr = FileRevision(dirname, basename)
-    getmodule(fr)
-    return render(io, fr, indent; variables...)
-end
-
-function render(io::IO, path::AbstractString; kwds...)
-    return render(io, basename(path), dirname(path); kwds...)
-end
+import ..Codegen: generate_haml_writer_codeblock, replace_output_nodes, @output, @io
 
 macro include(relpath, args...)
     relpath = esc(relpath)
@@ -82,7 +13,61 @@ macro include(relpath, args...)
     at_dir = getproperty(Base, Symbol("@__DIR__"))
     dir = macroexpand(__module__, Expr(:macrocall, at_dir, __source__))
 
-    :( render(@io, joinpath($dir, $relpath); variables=($(args...),)) )
+    :( render(joinpath($dir, $relpath); variables=($(args...),)) do (content...)
+        $(Expr(:hamloutput, :(content...)))
+    end )
+end
+
+"""
+    includehaml(mod::Module, fn::Symbol, path, indent="")
+
+Define two methods for the function `mod.fn` that allow rendering the HAML
+template in  the file `path`. These methods have the following signatures:
+
+    fn(io::IO; variables...)
+    fn(f::Function; variables...)
+
+where the output of the template will be written to `io` / passed to `f`
+respectively.
+"""
+includehaml(mod::Module, fn::Symbol, path, indent="") = _includehaml(mod, fn, path, indent)
+
+
+function _includehaml(mod::Module, fn::Symbol, path, indent="")
+    s = Source(path)
+    code = generate_haml_writer_codeblock(mod, s, string(indent))
+    code = replace_expression_nodes_unescaped(:hamloutput, code) do content...
+        :( f($(content...)) )
+    end
+    code = replace_expression_nodes_unescaped(:$, code) do sym
+        sym isa Symbol || error("Can only use variables as interpolations")
+        :( variables.data.$sym )
+    end
+    fn = esc(fn)
+    code = quote
+        $fn(f::Function; variables...) = $code
+        $fn(io::IO; variables...) = $fn(; variables...) do (content...)
+            write(io, content...)
+        end
+        $fn(; variables...) = let io = IOBuffer()
+            $fn(; variables...) do (content...)
+                write(io, content...)
+            end
+            String(take!(io))
+        end
+    end
+    code = make_hygienic(mod, code)
+    Base.eval(mod, code)
+end
+
+module Generated
+    import ...Templates: @output, @io, @include
+end
+
+function render(io, path; variables=(), indent="")
+    fn = gensym()
+    includehaml(Generated, fn, path, indent)
+    Base.invokelatest(getproperty(Generated, fn), io; variables...)
 end
 
 end # module
